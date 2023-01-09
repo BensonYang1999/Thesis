f4c33be9b972fb4c54f1d4c331cab6db7a047251
ChenMuHsi <louis19980927@gmail.com>
2023-01-09 11:35:09 +0000

    Use ViT to train image classifier on CIFAR10, TODO: modify the multi-head layer to my custom layer

--- ViT_head.py
+++ ViT_head.py
@@ -11,7 +11,6 @@ from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
 from torch.utils.data import DataLoader
 
-import torchvision
 from torchvision.transforms import ToTensor
 from torchvision.datasets.mnist import MNIST
 
@@ -25,12 +24,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--gpu", type=int, default=1)
 parser.add_argument("--bs", type=int, default=128)
 parser.add_argument("--add_freq", action="store_true", default=False)
-parser.add_argument("--epochs", type=int, default=1000)
-parser.add_argument("--lr", type=float, default=0.005)
 args = parser.parse_args()
 
 device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
 print("Using device: ", device, f"({torch.cuda.get_device_name(device)})" if torch.cuda.is_available() else "")
+bs = 128    
 
 def patchify(images, n_patches):
     n, c, h, w = images.shape
@@ -196,10 +194,8 @@ def main():
     # Loading data
     transform = ToTensor()
 
-    # train_set = MNIST(root='./../datasets', train=True, download=True, transform=transform)
-    # test_set = MNIST(root='./../datasets', train=False, download=True, transform=transform)
-    train_set = torchvision.datasets.CIFAR10(root='./../datasets', train=True, download=True, transform=transform)
-    test_set = torchvision.datasets.CIFAR10(root='./../datasets', train=False, download=True, transform=transform)
+    train_set = MNIST(root='./../datasets', train=True, download=True, transform=transform)
+    test_set = MNIST(root='./../datasets', train=False, download=True, transform=transform)
 
     train_loader = DataLoader(train_set, shuffle=True, batch_size=args.bs)
     test_loader = DataLoader(test_set, shuffle=False, batch_size=args.bs)
@@ -210,10 +206,9 @@ def main():
         print(f"*** ViT: Baseline + Frequency ***")
 
     # Defining model and training options
-    # model = MyViT((1, 28, 28), n_patches=7, n_blocks=2, hidden_d=8, n_heads=2, out_d=10).to(device)  # for MNIST
-    model = MyViT((3, 32, 32), n_patches=8, n_blocks=2, hidden_d=8, n_heads=2, out_d=10).to(device)  # for CIFAR10
-    N_EPOCHS = args.epochs
-    LR = args.lr
+    model = MyViT((1, 28, 28), n_patches=7, n_blocks=2, hidden_d=8, n_heads=2, out_d=10).to(device)
+    N_EPOCHS = 5
+    LR = 0.005
 
     # Training loop
     start_t = time.time()
