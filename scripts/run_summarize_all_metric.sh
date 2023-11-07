514e96a64a9e011c8edbdada887da0c4834bc9b2
ChenMuHsi <louis19980927@gmail.com>
2023-11-07 05:53:00 +0000

    [Delete] Delete the files that not used

---
+++ scripts/run_summarize_all_metric.sh
@@ -0,0 +1,40 @@
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0530_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k/test/2023-07-06_edge_25percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0530_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k/test/2023-07-06_edge_50percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0530_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k/test/2023-07-06_edge_75percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0530_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k/test/2023-07-06_edge_100percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0530_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k/test/2023-07-06_line_25percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0530_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k/test/2023-07-06_line_50percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0530_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k/test/2023-07-06_line_75percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0530_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k/test/2023-07-06_line_100percent
+
+
+
+
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0710_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k_frame-1_1_ReFFC_removed_last/2023-07-13_edge_25percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0710_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k_frame-1_1_ReFFC_removed_last/2023-07-13_edge_50percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0710_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k_frame-1_1_ReFFC_removed_last/2023-07-13_edge_75percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0710_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k_frame-1_1_ReFFC_removed_last/2023-07-13_edge_100percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0710_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k_frame-1_1_ReFFC_removed_last/2023-07-13_line_25percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0710_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k_frame-1_1_ReFFC_removed_last/2023-07-13_line_50percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0710_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k_frame-1_1_ReFFC_removed_last/2023-07-13_line_75percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ./results/0710_ZITS_video_YoutubeVOS_max500k_mix458k_turn470k_frame-1_1_ReFFC_removed_last/2023-07-13_line_100percent
+
+
+
+# python3 compute_score_summarize_fullDataset.py --input_dir ../FuseFormer/fuseformer_5frames_youtube_results/2023-07-06_edge_25percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ../FuseFormer/fuseformer_5frames_youtube_results/2023-07-06_edge_50percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ../FuseFormer/fuseformer_5frames_youtube_results/2023-07-06_edge_75percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ../FuseFormer/fuseformer_5frames_youtube_results/2023-07-06_edge_100percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ../FuseFormer/fuseformer_5frames_youtube_results/2023-07-06_line_25percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ../FuseFormer/fuseformer_5frames_youtube_results/2023-07-06_line_50percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ../FuseFormer/fuseformer_5frames_youtube_results/2023-07-06_line_75percent
+# python3 compute_score_summarize_fullDataset.py --input_dir ../FuseFormer/fuseformer_5frames_youtube_results/2023-07-06_line_100percent
+
+
+ python3 compare_sota.py --dataset line_50percent
+ python3 compare_sota.py --dataset line_75percent
+ python3 compare_sota.py --dataset line_100percent
+ python3 compare_sota.py --dataset edge_25percent
+ python3 compare_sota.py --dataset edge_50percent
+ python3 compare_sota.py --dataset edge_75percent
+ python3 compare_sota.py --dataset edge_100percent
