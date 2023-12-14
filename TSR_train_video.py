c9d40bf0514d1e549b3e64883cdebf32e8f72de6
ChenMuHsi <louis19980927@gmail.com>
2023-12-14 11:38:06 +0000

    [Fix] Fix the input width, height in the arguments

--- TSR_train_video.py
+++ TSR_train_video.py
@@ -94,10 +94,8 @@ if __name__ == '__main__':
     parser.add_argument('--validation_path', type=str, default=None, help='where is the validation set of ImageNet')
     # parser.add_argument('--val_line_path', type=str, default=None, help='Indicate where is the wireframes of val set')
     # parser.add_argument('--valid_mask_path', type=str, default=None)
-    # parser.add_argument('--image_w', type=int, default=432, help='input frame width')
-    # parser.add_argument('--image_h', type=int, default=240, help='input frame height')
-    parser.add_argument('--image_w', type=int, default=256, help='input frame width')
-    parser.add_argument('--image_h', type=int, default=256, help='input frame height')
+    parser.add_argument('--image_w', type=int, default=432, help='input frame width')
+    parser.add_argument('--image_h', type=int, default=240, help='input frame height')
     parser.add_argument('--image_size', type=int, default=256, help='input sequence length = image_size*image_size')
     parser.add_argument('--resume_ckpt', type=str, default='latest.pth', help='start from where, the default is latest')
     # Mask and predict finetune
